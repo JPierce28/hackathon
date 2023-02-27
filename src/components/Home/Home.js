8ca7e74d914c3f31ce48aa194f05685da0766a6d
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-02-27 10:09:56 -0700

    Add images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>
+    <section className='home-page'>
       <h1>hello</h1>
-    </div>
+    </section>
   )
 }
 
