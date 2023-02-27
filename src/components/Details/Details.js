372eb993619fbfd9024017f93e7518a162a77fd2
Josh Pierce <joshua.alan.pierce@gmail.com>
2023-02-27 11:59:52 -0700

    Add containter for details

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import './Details.css'
 import { useParams } from 'react-router-dom'
+import imageObjects from '../../mockData'
 
 const Details = () => {
   const { id } = useParams()
 
+  const currentImage = imageObjects.filter(image => {
+    return image.id == id
+  })
+
   return (
     <section className='details-page'>
-      <h1>Hello</h1>
+      <header className='details-header'>
+        <h1>art details</h1>
+      </header>
+      <div className='details-container'>
+        <h1>Container</h1>
+      </div>
     </section>
   )
 }
--- src/mockData.js
+++ src/mockData.js
@@ -2,32 +2,68 @@ const imageObjects = [
   {
     id: 1,
     image: '../images/alley.jpg',
-    artist: 'James Garman'
+    artist: 'James Garman',
+    location: 'Address Here',
+    socialMedia: {
+      artistWebsite: 'link',
+      instagram: 'link',
+      twitter: 'link',
+    }
   },
   {
     id: 2,
     image: '../images/banksy.jpg',
-    artist: 'Robinson Greig'
+    artist: 'Robinson Greig',
+    location: 'Address Here',
+    socialMedia: {
+      artistWebsite: 'link',
+      instagram: 'link',
+      twitter: 'link',
+    }
   },
   {
     id: 3,
     image: '../images/books.jpg',
-    artist: 'Kyle Glenn'
+    artist: 'Kyle Glenn',
+    location: 'Address Here',
+    socialMedia: {
+      artistWebsite: 'link',
+      instagram: 'link',
+      twitter: 'link',
+    }
   },
   {
     id: 4,
     image: '../images/einstein.jpg',
-    artist: 'Hannes Richter'
+    artist: 'Hannes Richter',
+    location: 'Address Here',
+    socialMedia: {
+      artistWebsite: 'link',
+      instagram: 'link',
+      twitter: 'link',
+    }
   },
   {
     id: 5,
     image: '../images/hallway.jpg',
-    artist: 'Hin Bong Yeung'
+    artist: 'Hin Bong Yeung',
+    location: 'Address Here',
+    socialMedia: {
+      artistWebsite: 'link',
+      instagram: 'link',
+      twitter: 'link',
+    }
   },
   {
     id: 6,
     image: '../images/stickers.jpg',
-    artist: 'Bruno Figueiredo'
+    artist: 'Bruno Figueiredo',
+    location: 'Address Here',
+    socialMedia: {
+      artistWebsite: 'link',
+      instagram: 'link',
+      twitter: 'link',
+    }
   }
 ]
 
